04fd57b0d63d62bc5261206efcba3bfa3d6663c6
CourtneyG <courtney.glosser@appliedfundamentals.net>
2025-02-03 08:28:35 -0500

    Trying to setup Vite file watching and auto-compilation. WIP

--- vampire_survivors_tut/vite.config.js
+++ vampire_survivors_tut/vite.config.js
@@ -10,6 +10,14 @@ export default defineConfig({
                 manualChunks: undefined // Keeps everything in one JS file
             }
         }
-    }
+    },
+    server: {
+        watch: {
+            usePolling: true
+        },
+    },
+    esbuild: {
+        loader: "ts",
+    },
 });
 
